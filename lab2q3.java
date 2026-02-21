002adf06cd776e05c5f3e5de3ba77e56aa344226
MEER MUJTABA <136008101+MeerMujtaba@users.noreply.github.com>
2026-02-21 23:07:23 +0500

    Add files via upload

---
+++ lab2q3.java
@@ -0,0 +1,9 @@
+public class lab2q3 {
+    public static void main(String[] args) {
+        float Length=7.4f, Width=4.2f;
+        float area=Length*Width;
+        System.out.println("Area of the rectangle is "+area);
+    }
+}
+    
+
