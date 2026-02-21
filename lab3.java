a25a4e344dadd80ce462a141c0421410c2a094a9
MEER MUJTABA <136008101+MeerMujtaba@users.noreply.github.com>
2026-02-21 23:05:21 +0500

    Add files via upload

---
+++ lab3.java
@@ -0,0 +1,11 @@
+public class lab3 {
+    public static void main(String[] args) {
+        long salary = 50000;
+        System.out.println("Salary: "+salary);
+        for(int i=1; i<=5; i++){
+            System.out.println("After "+i+" Years: "+(salary+i*2000));
+        }
+    }
+}
+    
+
