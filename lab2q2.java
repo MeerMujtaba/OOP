839cecb91de5a895848e25339a81dbbcbedd18ed
MEER MUJTABA <136008101+MeerMujtaba@users.noreply.github.com>
2026-02-21 23:04:48 +0500

    Add files via upload

---
+++ lab2q2.java
@@ -0,0 +1,12 @@
+public class lab2q2 {
+   public static void main(String[] args) {
+    int a=72, b=18;
+    System.out.println("Their addition is: "+(a+b));
+    System.out.println("Their subtraction is: "+(a-b));
+    System.out.println("Their multiplication is: "+(a*b));
+    System.out.println("Their division is: "+(a/b));
+    System.out.println("Their modulus is: "+(a%b));
+   } 
+}
+    
+
